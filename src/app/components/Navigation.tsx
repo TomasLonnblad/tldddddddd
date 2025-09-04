f3ce5f6344287cd53c47f9d5f799fc853527808a
Tomas <tomaslonnblad@gmail.com>
2025-09-04 21:07:26 +0200

    Update Navigation.tsx

--- src/app/components/Navigation.tsx
+++ src/app/components/Navigation.tsx
@@ -3,7 +3,7 @@ import TransitionLink from "./TransitionLink"
 const Navigation = () => {
   return (
     <nav className="w-full p-5 flex flex-row place-items-center justify-between">
-      <h1 className="text-3xl tracking-tight font-bold text-900">
+      <h1 className="text-3xl tracking-tight font-bold text-neutral-900">
         rithmic
       </h1>
       <div className="flex gap-5">
