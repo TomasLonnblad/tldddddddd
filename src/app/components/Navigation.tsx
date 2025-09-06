994875e74f1825b2f306f400a45c5622bbef64d7
Tomas <tomaslonnblad@gmail.com>
2025-09-06 09:31:16 +0200

    Update Navigation.tsx

--- src/app/components/Navigation.tsx
+++ src/app/components/Navigation.tsx
@@ -3,10 +3,15 @@ import TransitionLink from "./TransitionLink"
 const Navigation = () => {
   return (
     <nav className="w-full p-5 flex flex-row place-items-center justify-between">
-      <h1 className="text-3xl tracking-tight font-bold text-neutral-900">
+      <h1 className="text-3xl tracking-tight font-bold text-neutral-400">
         rithmic
       </h1>
-      <div className="flex gap-5">
+      <div className="
+
+       
+       bg-clip-padding p-4 m-4 border-8 border-primary border-dotted w-32 h-32 rounded-xl bg-tertiary">
+       
+       
         <TransitionLink href="/" label="Home" />
         <TransitionLink href="/work" label="Work" />
         <TransitionLink href="/about" label="About" />
